69781ccb96a276f4f2d8673128b5868967231bf8
iizaghz <sukmahawaiza@gmail.com>
2026-03-12 11:51:21 +0700

    Add tombol Hitung Mhs

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -55,7 +55,24 @@ namespace PraktikumADO
 
         private void btnHitungMhs_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnHitungMK_Click(object sender, EventArgs e)
